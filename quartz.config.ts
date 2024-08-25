99e96227e37e93c18ff9684a4d6c4ea8db8112b3
Christopher Arausa <carausa@magi.ai>
2024-08-25 18:07:23 -0400

    Quartz sync: Aug 25, 2024, 6:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -71,6 +71,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.OxHugoFlavouredMarkdown()
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
